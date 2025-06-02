7ff1f8ea24d5dede9d60599f0338dc6913ec06a3
Gustavo Moura <gustavo.moura-dev@outlook.com>
2025-06-02 13:56:57 -0300

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var flor = document.getElementById("flor")
+var carta = document.getElementById("carta")
+var cartaImagem = document.getElementById("cartaImagem")
+
+function ativarFlor(){
+    flor.style.opacity = "100%"
+    flor.style.display = "block"
+    carta.style.display = "block"
+}
+
+function ativarCarta(){
+    cartaImagem.style.display = "block"
+}
